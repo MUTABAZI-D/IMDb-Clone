db39f935b07eb7468f575839bcd51bd6817b1fc6
MUTABAZI Dieudonne <mudonne18@gmail.com>
2024-12-06 13:58:33 +0200

    modified navbar to enable seamless experience

--- src/components/NavbarItem.tsx
+++ src/components/NavbarItem.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import { useSearchParams } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
 
 type Props = {
   title: string;
@@ -9,16 +9,19 @@ type Props = {
 
 export const NavbarItem = ({ title, param }: Props) => {
   const searchParams = useSearchParams();
+  const pathname = usePathname();
   const genre = searchParams.get("genre");
+
   return (
     <div>
       <Link
         href={`/?genre=${param}`}
         className={`hover:text-amber-600 font-semibold ${
+          (pathname === "/" && param === "fetchTopRated" && !genre) ||
           genre === param
             ? "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
             : ""
-        }`}
+        } `}
       >
         {title}
       </Link>
